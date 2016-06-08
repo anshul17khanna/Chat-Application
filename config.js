3142578fcb36b06db40ae120e2a7a5dece1f15b7
Anshul Khanna <anshul17khanna@gmail.com>
2016-06-08 11:35:37 +0530

    initial setup

---
+++ config.js
@@ -0,0 +1,14 @@
+// This file handles the configuration of the app.
+var express = require('express');
+
+module.exports = function(app, io){
+
+	app.set('view engine', 'html');
+
+	app.engine('html', require('ejs').renderFile);
+
+	app.set('views', __dirname + '/views');
+
+	app.use(express.static(__dirname + '/public'));
+
+};
